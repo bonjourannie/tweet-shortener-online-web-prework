e4f94935d7e867cce8ce6fde03d394a661e78993
Annie Truex <artruex@gmail.com>
2019-06-03 01:16:56 +0000

    Automatically backed up by Learn

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -1,5 +1,5 @@
 # Write your code here.
-def word_substituter(tweet)
+def word_substituter(tweet_one)
     tweet.split(" ").map do |word|
       if dictionary.keys.include?(word.downcase)
         word = dictionary[word.downcase]
