08cf41f6d4b6964e46771b241770751f1fd71357
Annie Truex <artruex@gmail.com>
2019-06-03 01:33:28 +0000

    Automatically backed up by Learn

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -31,9 +31,13 @@ def bulk_tweet_shortener(tweets)
 end
 
 def selective_tweet_shortener (tweet)
-if tweet.length > 140
-  word_substituter(tweet)
-else 
- tweet
+  if tweet.length > 140
+    word_substituter(tweet)
+  else 
+    tweet
+  end
 end
-end
+
+def shortened_tweet_truncator(tweet)
+  if tweet.length > 140
+    
