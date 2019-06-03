52550e877e1453771272f784b75b9934316c3820
Annie Truex <artruex@gmail.com>
2019-06-03 01:25:33 +0000

    Automatically backed up by Learn

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -32,7 +32,7 @@ end
 
 def selective_tweet_shortener(tweets)
     tweets.split(" ").map do |phrase|
-    if phrase.length > 130
+    if phrase.length > 140
       word_substituter(phrase)
     elsif phrase.length = 130
       phrase
