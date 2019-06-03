42d1aba12153063e488d1089d5a3102f4349568f
Annie Truex <artruex@gmail.com>
2019-06-03 01:11:35 +0000

    Automatically backed up by Learn

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -1,2 +1,17 @@
 # Write your code here.
-def word_substitutor (tweet)
+def dictionary
+  convert = {
+   "hello" => "hi",
+   "to" => "2",
+   "two" => "2",
+   "too" => "2",
+   "for" => "4",
+   "four" => "4",
+   "be" => "b",
+   "you" => "u",
+   "at" => "@",
+   "and" => "&"
+}
+end
+
+#def word_substitutor (tweet)
